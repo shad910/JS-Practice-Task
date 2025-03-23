465b7981459f797c2997bffb34e383c696b411f4
shad910 <shadshs91@gmail.com>
2025-03-23 14:43:34 +0600

    React Practice

--- js-react/src/App.jsx
+++ js-react/src/App.jsx
@@ -1,35 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Rect Core Practice</h1>
+      <Form name="Shad" roll="770***" reg="1502288***"></Form>
+      <FavFood fruit="Orange" fastFood="Burger"></FavFood>
+      <Form name="Monayem" roll="745***" reg="1502****"></Form>
+      <FavFood fruit="Orange" fastFood="Pizza"></FavFood>
+      <Form name="Evan" roll="770***" reg="15022***"></Form>
+      <FavFood fruit="Orange" fastFood="Latus Pata"></FavFood>
+      <Form name="Abir" roll="770***" reg="1502288**"></Form>
+      <FavFood fruit="Orange" fastFood="Pasta"></FavFood>
     </>
+  );
+}
+
+function Form(props) {
+  console.log(props);
+  return (
+    <section className="container">
+      <div className="section1">
+        <h3>Fill up the form:</h3>
+        <div className="div1">
+          <p className="ps1">Name: {props.name}</p>
+          <p className="ps1">Roll: {props.roll}</p>
+          <p className="ps1">Reg: {props.reg}</p>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+function FavFood ({fruit,fastFood}) {
+  return (
+    <section className="section2">
+      <ul className="ps1">
+        <li>Fruits: {fruit} </li>
+        <li>Fast Food: {fastFood} </li>
+      </ul>
+    </section>
   )
 }
 
-export default App
+export default App;
